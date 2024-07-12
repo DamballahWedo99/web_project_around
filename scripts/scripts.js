3e13c6499c654097f1c7cac2674d9fc068e15d71
Emilio Casso <damballahwedo911@gmail.com>
2024-07-12 12:40:59 -0600

    fix: reacomoda y simplifica JS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,54 +1,48 @@
-// //Tarjetas iniciales cuando se carga la página
-document.addEventListener("DOMContentLoaded", function () {
-  const initialCards = [
-    {
-      name: "Valle de Yosemite",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
-    },
-    {
-      name: "Lago Louise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
-    },
-    {
-      name: "Montañas Calvas",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
-    },
-    {
-      name: "Latemar",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
-    },
-    {
-      name: "Parque Nacional de la Vanoise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
-    },
-    {
-      name: "Lago di Braies",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
-    },
-  ];
+// Variables
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
 
-  const cardTemplate = document.querySelector("#card-template").content;
-
-  initialCards.forEach((card) => {
-    const cardElement = cardTemplate
-      .querySelector(".elements__container")
-      .cloneNode(true);
-    cardElement.querySelector(".elements__image").src = card.link;
-    cardElement.querySelector(".elements__image").alt = card.name;
-    cardElement.querySelector(".elements__name").textContent = card.name;
-
-    document.querySelector(".elements").append(cardElement);
-  });
-});
-
-// Da funcionalidad para abrir el editor de perfil + agregar tarjetas
-const popupProfile = document.querySelector(".popup_profile");
-const popupCards = document.querySelector(".popup_cards");
+const cardTemplate = document.querySelector("#card-template");
+const popupProfile = document.querySelector("#popup-profile");
+const popupCards = document.querySelector("#popup-cards");
 const profileButton = document.querySelector(".profile__edit-button");
-const cardButton = document.querySelectorAll(".profile__add-button");
+const cardButton = document.querySelector(".profile__add-button-container");
 const closeButtonProfile = document.querySelector(".popup__close-icon_profile");
 const closeButtonCards = document.querySelector(".popup__close-icon_cards");
+const profileName = document.querySelector(".profile__name");
+const inputName = document.querySelector(".popup__input_name");
+const profileAbout = document.querySelector(".profile__job");
+const inputAbout = document.querySelector(".popup__input_about");
+const submitButtonProfile = document.querySelector("#submit-profile");
+const submitButtonCard = document.querySelector("#submit-card");
+const inputTitle = document.querySelector(".popup__input_title");
+const inputImageUrl = document.querySelector(".popup__input_image-url");
 
+// funciones
 function toggleProfile() {
   popupProfile.classList.toggle("popup_opened");
 }
@@ -57,19 +51,28 @@ function toggleCards() {
   popupCards.classList.toggle("popup_opened");
 }
 
-profileButton.addEventListener("click", toggleProfile);
-cardButton.forEach((button) => {
-  button.addEventListener("click", toggleCards);
+function addCard(card) {
+  const cardElement = cardTemplate
+    .cloneNode(true)
+    .content.querySelector(".elements__container");
+  cardElement.querySelector(".elements__image").src =
+    card.link || inputImageUrl.value;
+  cardElement.querySelector(".elements__image").alt = card.name;
+  cardElement.querySelector(".elements__name").textContent =
+    card.name || inputTitle.value;
+
+  document.querySelector(".elements").prepend(cardElement);
+}
+
+// Eventlisteners
+initialCards.forEach((card) => {
+  addCard(card);
 });
+
+profileButton.addEventListener("click", toggleProfile);
+cardButton.addEventListener("click", toggleCards);
 closeButtonProfile.addEventListener("click", toggleProfile);
 closeButtonCards.addEventListener("click", toggleCards);
-
-// Da funcionalidad para modificar el perfil
-const profileName = document.querySelector(".profile__name");
-const inputName = document.querySelector(".popup__input_name");
-const profileAbout = document.querySelector(".profile__job");
-const inputAbout = document.querySelector(".popup__input_about");
-const submitButtonProfile = document.querySelector("#submit-profile");
 
 submitButtonProfile.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -83,27 +86,14 @@ profileButton.addEventListener("click", () => {
   inputAbout.value = profileAbout.textContent;
 });
 
-// Da funcionalidad de agregar tarjetas
-const submitButtonCard = document.querySelector("#submit-card");
-
 submitButtonCard.addEventListener("click", function (evt) {
   evt.preventDefault();
-  const inputTitle = document.querySelector(".popup__input_title");
-  const inputImageUrl = document.querySelector(".popup__input_image-url");
-  const cardTemplate = document.querySelector("#card-template").content;
-  const cardElement = cardTemplate
-    .querySelector(".elements__container")
-    .cloneNode(true);
-  cardElement.querySelector(".elements__image").src = inputImageUrl.value;
-  cardElement.querySelector(".elements__image").alt = inputTitle.value;
-  cardElement.querySelector(".elements__name").textContent = inputTitle.value;
-  document.querySelector(".elements").prepend(cardElement);
+  addCard(evt);
   inputTitle.value = "";
   inputImageUrl.value = "";
   toggleCards();
 });
 
-// Da funcionalidad de remover tarjetas
 document.querySelector(".elements").addEventListener("click", function (evt) {
   if (evt.target.classList.contains("elements__trash-can")) {
     const card = evt.target.closest(".elements__container");
