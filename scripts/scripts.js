aaf71494abfca5446523592f14c05cda3d2f3886
Emilio Casso <damballahwedo911@gmail.com>
2024-08-07 19:23:19 -0600

    fix(JS): refactoriza handlers que abren y cierran ventanas modales

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,13 @@
 // Variables
 import Card from "./card.js";
 import FormValidator from "./formValidator.js";
+import {
+  profileHandler,
+  cardHandler,
+  imageHandler,
+  toggleClick,
+  toggleEscape,
+} from "./utils.js";
 
 const initialCards = [
   {
@@ -42,65 +49,6 @@ const formValidator = new FormValidator(
 );
 
 const elements = document.querySelector(".elements");
-const popupProfile = document.querySelector("#popup-profile");
-const popupCards = document.querySelector("#popup-cards");
-const popupImage = document.querySelector("#popup-image");
-const popupImageTitle = document.querySelector(".popup__image-title");
-const popupFullImage = document.querySelector(".popup__image");
-const profileButton = document.querySelector(".profile__edit-button");
-const cardButton = document.querySelector(".profile__add-button-container");
-const closeButtonProfile = document.querySelector(".popup__close-icon_profile");
-const closeButtonCards = document.querySelector(".popup__close-icon_cards");
-const closeButtonImage = document.querySelector("#image-close-icon");
-const profileName = document.querySelector(".profile__name");
-const inputName = document.querySelector("#name");
-const profileAbout = document.querySelector(".profile__job");
-const inputAbout = document.querySelector("#about");
-const submitButtonProfile = document.querySelector("#submit-profile");
-const submitButtonCard = document.querySelector("#submit-card");
-const inputTitle = document.querySelector("#title");
-const inputImageUrl = document.querySelector("#image-url");
-
-// funciones
-function toggleProfile() {
-  popupProfile.classList.toggle("popup_opened");
-  formValidator.resetValidation();
-}
-
-function toggleCards() {
-  popupCards.classList.toggle("popup_opened");
-  inputTitle.value = "";
-  inputImageUrl.value = "";
-  formValidator.resetValidation();
-}
-
-function toggleImage() {
-  popupImage.classList.toggle("popup_opened");
-}
-
-const popupRemove = () => {
-  document.querySelectorAll(".popup").forEach((popup) => {
-    popup.classList.remove("popup_opened");
-  });
-};
-
-const toggleEscape = (evt) => {
-  if (evt.key === "Escape") {
-    popupRemove();
-  }
-};
-
-const toggleClick = () => {
-  const popupList = Array.from(document.querySelectorAll(".popup"));
-
-  popupList.forEach((popup) => {
-    popup.addEventListener("click", function (evt) {
-      if (!evt.target.closest(".popup__container")) {
-        popupRemove();
-      }
-    });
-  });
-};
 
 // Eventlisteners
 initialCards.forEach((formData) => {
@@ -109,50 +57,10 @@ initialCards.forEach((formData) => {
   elements.prepend(cardElement);
 });
 
-submitButtonCard.addEventListener("click", function (evt) {
-  evt.preventDefault();
-
-  const cardData = {
-    name: inputTitle.value,
-    link: inputImageUrl.value,
-  };
-
-  const card = new Card(cardData, "#card-template");
-  const cardElement = card.generateCard();
-
-  elements.prepend(cardElement);
-  inputTitle.value = "";
-  inputImageUrl.value = "";
-  toggleCards();
-});
-
 formValidator.enableValidation();
 
-profileButton.addEventListener("click", toggleProfile);
-cardButton.addEventListener("click", toggleCards);
-closeButtonProfile.addEventListener("click", toggleProfile);
-closeButtonCards.addEventListener("click", toggleCards);
-closeButtonImage.addEventListener("click", toggleImage);
-
-submitButtonProfile.addEventListener("click", function () {
-  profileName.textContent = inputName.value;
-  profileAbout.textContent = inputAbout.value;
-  toggleProfile();
-});
-
-profileButton.addEventListener("click", () => {
-  inputName.value = profileName.textContent;
-  inputAbout.value = profileAbout.textContent;
-});
-
-elements.addEventListener("click", function (evt) {
-  if (evt.target.classList.contains("elements__trash-can")) {
-    const card = evt.target.closest(".elements__container");
-    if (card) {
-      card.remove();
-    }
-  }
-});
-
-document.addEventListener("keydown", toggleEscape);
+profileHandler();
+cardHandler(elements);
+imageHandler();
+toggleEscape();
 toggleClick();
---
+++ scripts/utils.js
@@ -0,0 +1,133 @@
+import Card from "./card.js";
+import FormValidator from "./formValidator.js";
+
+const formValidator = new FormValidator(
+  {
+    inputSelector: ".popup__input",
+    fieldsetSelector: ".popup__info-field",
+    submitButtonSelector: ".popup__button",
+    inactiveButtonClass: "popup__button_disabled",
+    inputErrorClass: "popup__input_type_error",
+    errorClass: "popup__input-error_active",
+  },
+  ".popup__form"
+);
+
+const popupProfile = document.querySelector("#popup-profile");
+const profileButton = document.querySelector(".profile__edit-button");
+const closeButtonProfile = document.querySelector(".popup__close-icon_profile");
+const profileAbout = document.querySelector(".profile__job");
+const profileName = document.querySelector(".profile__name");
+const inputName = document.querySelector("#name");
+const inputAbout = document.querySelector("#about");
+const submitButtonProfile = document.querySelector("#submit-profile");
+
+const popupCards = document.querySelector("#popup-cards");
+const cardButton = document.querySelector(".profile__add-button-container");
+const closeButtonCards = document.querySelector(".popup__close-icon_cards");
+const submitButtonCard = document.querySelector("#submit-card");
+const inputTitle = document.querySelector("#title");
+const inputImageUrl = document.querySelector("#image-url");
+
+const popupImage = document.querySelector("#popup-image");
+const closeButtonImage = document.querySelector("#image-close-icon");
+
+// Profile
+function toggleProfile() {
+  popupProfile.classList.toggle("popup_opened");
+}
+
+const profileHandler = () => {
+  profileButton.addEventListener("click", () => {
+    toggleProfile();
+    inputName.value = profileName.textContent;
+    inputAbout.value = profileAbout.textContent;
+  });
+
+  closeButtonProfile.addEventListener("click", () => {
+    toggleProfile();
+    formValidator.resetValidation();
+  });
+
+  submitButtonProfile.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileAbout.textContent = inputAbout.value;
+    toggleProfile();
+  });
+};
+
+// Card
+function toggleCard() {
+  popupCards.classList.toggle("popup_opened");
+}
+
+const cardHandler = (elements) => {
+  cardButton.addEventListener("click", () => {
+    toggleCard();
+  });
+
+  closeButtonCards.addEventListener("click", () => {
+    toggleCard();
+    inputTitle.value = "";
+    inputImageUrl.value = "";
+    formValidator.resetValidation();
+  });
+
+  submitButtonCard.addEventListener("click", function (evt) {
+    evt.preventDefault();
+
+    const cardData = {
+      name: inputTitle.value,
+      link: inputImageUrl.value,
+    };
+
+    const card = new Card(cardData, "#card-template");
+    const cardElement = card.generateCard();
+
+    elements.prepend(cardElement);
+    inputTitle.value = "";
+    inputImageUrl.value = "";
+    toggleCard();
+  });
+};
+
+// image
+function toggleImage() {
+  popupImage.classList.toggle("popup_opened");
+}
+
+const imageHandler = () => {
+  closeButtonImage.addEventListener("click", () => {
+    toggleImage();
+  });
+};
+
+// keyboard and outside click
+const popupRemove = () => {
+  document.querySelectorAll(".popup").forEach((popup) => {
+    popup.classList.remove("popup_opened");
+  });
+};
+
+const toggleEscape = () => {
+  document.addEventListener("keydown", (evt) => {
+    if (evt.key === "Escape") {
+      popupRemove();
+    }
+  });
+};
+
+const toggleClick = () => {
+  const popupList = Array.from(document.querySelectorAll(".popup"));
+
+  popupList.forEach((popup) => {
+    popup.addEventListener("click", function (evt) {
+      if (!evt.target.closest(".popup__container")) {
+        popupRemove();
+      }
+    });
+  });
+};
+
+export { profileHandler, cardHandler, imageHandler, toggleClick, toggleEscape };
